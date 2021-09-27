3fb0d3a1707de52b6096ff8efb3fd73248f610d3
Sergey Filatov <3037677@gmail.com>
2021-09-27 11:44:25 +0300

    add L1-task2

---
+++ Lesson-01/task-2(Nested_elements_with_React)/index.js
@@ -0,0 +1,36 @@
+// const rootElement = document.querySelector('#root');
+
+// const renderGreeting = (elem) => {
+//   const containerElem = document.createElement('div');
+//   containerElem.classList.add('greeting');
+
+//   const titleElem = document.createElement('h1');
+//   titleElem.classList.add('greeting__title');
+//   titleElem.textContent = 'Hello, world!';
+
+//   const textElem = document.createElement('p');
+//   textElem.classList.add('greeting__text');
+//   textElem.textContent = "I'm learning React";
+
+//   containerElem.append(titleElem, textElem);
+
+//   elem.append(containerElem);
+// };
+
+// renderGreeting(rootElement);
+const rootElement = document.querySelector('#root');
+
+const element = React.createElement('div', { className: 'greeting' }, 
+  React.createElement(
+    'div', 
+    { className: 'greeting__title' }, 
+    'Hello, world!'
+    ),
+  React.createElement(
+    'div',
+    { className: 'greeting__text' },
+    "I'm learning React"
+  ),
+);
+
+ReactDOM.render(element,rootElement);
