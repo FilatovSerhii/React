1dd4bbfa6340b547b529d709c3736b7983511bec
Sergey Filatov <3037677@gmail.com>
2021-09-29 01:45:52 +0300

    1

--- Lesson-04/hw1/src/App.jsx
+++ Lesson-04/hw1/src/App.jsx
@@ -4,8 +4,8 @@ import Clock from './Clock.jsx';
 const App = () => {
   return (
     <>
-      <Clock location="New York" offset={-5} />
-      <Clock location="Kyiv" offset={2} />
+      <Clock location="New York" offset={5} />
+      <Clock location="Kyiv" offset={-2} />
       <Clock location="London" offset={0} />
     </>
   );
