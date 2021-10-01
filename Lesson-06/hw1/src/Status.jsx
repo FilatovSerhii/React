35caba597a9a5719c1dfc8b81d8368604ebec60a
Sergey Filatov <3037677@gmail.com>
2021-10-01 16:53:15 +0300

    2

--- Lesson-06/hw1/src/Status.jsx
+++ Lesson-06/hw1/src/Status.jsx
@@ -3,11 +3,13 @@ import Online from './Online.jsx';
 import Offline from './Offline.jsx';
 
 const Status = ({ isOnline }) => {  
+  return (
     <div className="status">
       {isOnline ? 
       <Online /> 
       : <Offline />}
     </div>
+  );
 };
 
 export default Status;
