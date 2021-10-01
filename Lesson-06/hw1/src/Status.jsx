54c326e67b38a08ada3d6d776f5d814cc537e650
Sergey Filatov <3037677@gmail.com>
2021-10-01 16:50:58 +0300

    1

--- Lesson-06/hw1/src/Status.jsx
+++ Lesson-06/hw1/src/Status.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import Online from './Online.jsx';
 import Offline from './Offline.jsx';
 
-const Status = ({ isOnline = true }) => {  // имитация статуса online
-  return (
+const Status = ({ isOnline }) => {  
     <div className="status">
       {isOnline ? 
       <Online /> 
       : <Offline />}
     </div>
-  );
 };
 
 export default Status;
