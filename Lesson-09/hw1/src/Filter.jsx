3f543d67654295c1140a5324f5b778758bb761c2
Sergey Filatov <3037677@gmail.com>
2021-10-05 01:43:35 +0300

    1

--- Lesson-09/hw1/src/Filter.jsx
+++ Lesson-09/hw1/src/Filter.jsx
@@ -11,7 +11,7 @@ const Filter = ({filterText, count, onChange}) => {
   return (<>
             <span className="filter__count">{count}</span>
             <input 
-              // type="text" 
+              type="text" 
               className="filter__input" 
               // value={filterText} 
               onChange={onChange}
