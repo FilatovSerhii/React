fef85aef75a2093531173bac40667bc2e00e62d0
Sergey Filatov <3037677@gmail.com>
2021-10-17 01:11:52 +0300

    2

--- Lesson-13/task1/src/App.jsx
+++ Lesson-13/task1/src/App.jsx
@@ -5,17 +5,27 @@ import Home from './Home';
 import Products from './Products';
 import PageNotFound from './PageNotFound';
 
-const App = () => (
-  <div className="page">
-    <BrowserRouter>
-    <Switch>
-      <Route exact path="/"> <Home /> </Route>
-      <Route path="/products"> <Products /> </Route>
-      <Route path="/contacts"> <Contacts /> </Route>
-      <Route path="*"> <PageNotFound /> </Route>
-    </Switch>
-     </BrowserRouter >
-  </div>
+const App = () => {
+  return (
+    <div className="page">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/products">
+            <Products />
+          </Route>
+          <Route path="/contacts">
+            <Contacts />
+          </Route>
+          <Route path="*">
+            <PageNotFound />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
+};
 
 export default App;
