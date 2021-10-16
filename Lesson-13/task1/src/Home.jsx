871ae7482adf27e063b4d945b9143723050cff57
Sergey Filatov <3037677@gmail.com>
2021-10-17 01:15:01 +0300

    3

--- Lesson-13/task1/src/Home.jsx
+++ Lesson-13/task1/src/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Home() {
+const Home = () => {
   return (
     <div className="page__content">
       <h1>🏠</h1>
-      <Link to="/products">To Products</Link>
+      <Link to="/products">To products</Link>
     </div>
   );
-}
+};
 
 export default Home;
