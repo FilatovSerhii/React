7b55329ba3e406b57b06282f516231a40fc58e30
Sergey Filatov <3037677@gmail.com>
2021-10-17 11:56:20 +0300

    1

--- Lesson-13/task2/src/Product.jsx
+++ Lesson-13/task2/src/Product.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useParams} from 'react-router-dom'
 
-function Product() {
+const Product = () => {
  const {productId} = useParams
   return (
     <div className="product">
