cd40e2968888d9014add06d17909fcb979e60923
Sergey Filatov <3037677@gmail.com>
2021-10-17 23:45:21 +0300

    add task2

---
+++ Lesson-14/task2/src/User.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+const User = ({ match }) => {
+  const [userData, setUserData] = useState(null);
+  const { userId } = useParams();
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${userId}`)
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+
+        throw new Error();
+      })
+      .then(userData => {
+        setUserData(userData);
+      });
+  }, [userId]);
+
+  if (!userData) {
+    return null;
+  }
+
+  const { avatar_url, name, location } = userData;
+  return (
+    <div className="user">
+      <img alt="User Avatar" src={avatar_url} className="user__avatar" />
+      <div className="user__info">
+        <span className="user__name">{name}</span>
+        <span className="user__location">{location}</span>
+      </div>
+    </div>
+  );
+};
+
+export default User;
